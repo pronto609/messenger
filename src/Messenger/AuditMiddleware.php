219790e63782fb31aef293c8ce439e6139ec7703
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-27 08:32:19 +0300

    Tracking Messages with Middleware & a Stamp

--- src/Messenger/AuditMiddleware.php
+++ src/Messenger/AuditMiddleware.php
@@ -5,12 +5,21 @@ namespace App\Messenger;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
+use App\Messenger\UniqueIdStamp;
 
 class AuditMiddleware implements MiddlewareInterface
 {
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
+        if (null === $envelope->last(UniqueIdStamp::class)) {
+            $envelope = $envelope->with(new UniqueIdStamp());
+        }
+
+        /** @var UniqueIdStamp $stamp */
+        $stamp = $envelope->last(UniqueIdStamp::class);
+        dump($stamp->getUniqueId());
+
         return $stack->next()->handle($envelope, $stack);
     }
 }
